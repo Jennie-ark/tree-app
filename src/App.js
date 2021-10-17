d167ca1d2e927502c1189fb0bd832ac89e7e1a54
Jennie Nii <anriasan@gmail.com>
2021-10-17 17:22:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const tree = {
+  name: "Tree",
+  child: [
+    {
+      name: "/",
+      child: [
+        {
+          name: "Home",
+          child: [],
+        },
+        {
+          name: "Lib",
+          child: [
+            {
+              name: "bin",
+              child: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Host",
+      child: [],
+    },
+  ],
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Tree />
+      <button onClick={refreshTree}>Click to reload!</button>
+    </>
   );
 }
 
-export default App;
+function refreshTree() {
+  window.location.reload(false);
+}
+
+function saveTree(tree) {
+  console.log(tree);
+}
+
+function Tree(props) {
+  const isRoot = !props.node;
+
+  const { onRemove, onInsert, isLastChild, onUpdate } = props;
+  const [node, setNode] = useState(props.node || tree);
+
+  useEffect(() => {
+    if (isRoot) {
+      saveTree(node);
+    } else {
+      onUpdate(node);
+    }
+  }, [node]);
+
+  function addChild(name) {
+    setNode({
+      ...node,
+      child: [...node.child, { name, child: [] }],
+    });
+  }
+
+  function removeChildAtIdx(idx) {
+    setNode({
+      ...node,
+      child: node.child.filter((_, i) => i !== idx),
+    });
+  }
+  function updateChild(obj) {
+    setNode({
+      ...node,
+      child: node.child.map((el) => {
+        if (el.name === obj.name) {
+          return obj;
+        } else {
+          return el;
+        }
+      }),
+    });
+  }
+
+  return (
+    <div className="wrapper">
+      <ul>
+        <li>
+          {node.name}
+          {!isRoot && (
+            <span onClick={onRemove}>
+              <button>Remove</button>
+            </span>
+          )}
+
+          {isLastChild && (
+            <input
+              placeholder="press 'Enter' for add"
+              maxLength="20"
+              onKeyUp={(e) => {
+                if (e.key === "Enter") onInsert(e.target.value);
+              }}
+            />
+          )}
+        </li>
+        {node.child.length > 0 && (
+          <li>
+            {node.child.map((el, idx) => (
+              <Tree
+                key={el.name}
+                node={el}
+                onRemove={() => removeChildAtIdx(idx)}
+                onInsert={(name) => addChild(name)}
+                isLastChild={idx === node.child.length - 1}
+                onUpdate={(node) => updateChild(node)}
+              />
+            ))}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
